319fefd2902be873c60303f2bc8f09f4d8fb1596
kimak <mdumand@molotov.tv>
2016-02-16 10:37:31 +0100

    fix(app): clean

--- web_modules/app/App.js
+++ web_modules/app/App.js
@@ -1,42 +1,7 @@
 import React, { Component } from 'react';
-import fetchJSON from "./fetchJSON";
-import List from "List";
-import consts from "./consts"
-
-import ItemDetails from "ItemDetails"
 import HeaderNav from "HeaderNav"
 
 export default class App extends Component {
-
-  state = {
-    artists: null,
-    kinds: null,
-  };
-
-  fetchArtist(name){
-    fetchJSON(consts.api.enpoints.getSearch(name,"artist")).then((response) => {
-        if(!response.error){
-          this.setState({artists:response.artists.items})
-        }
-    });
-  };
-
-  fetchKinds(){
-    fetchJSON(consts.api.enpoints.getKinds()).then((response) => {
-        if(!response.error){
-          this.setState({kinds:response.genres})
-        }
-    });
-  };
-
-
-  onInputArtistChange = (value) => {
-      this.fetchArtist(value);
-  };
-
-  componentDidMount() {
-      this.fetchKinds();
-  };
 
   render() {
     return (
